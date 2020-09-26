972f9fcefd162bec918a736f64adc2e41ae1e405
Aaron Alberto Alva Caffo <u201719762@upc.edu.pe>
2020-09-26 18:46:43 -0500

    ResourceTomodelProfil

--- Mapping/ModelToResourceProfile.cs
+++ Mapping/ModelToResourceProfile.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Homemade.Mapping
 {
-    public class ModelToResourceProfile
+    public class ModelToResourceProfile : Profile
     {
+        
     }
 }
--- Mapping/ResourceToModelProfile.cs
+++ Mapping/ResourceToModelProfile.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using AutoMapper;
+using Homemade.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Homemade.Mapping
 {
-    public class ResourceToModelProfile
+    public class ResourceToModelProfile : Profile
     {
+        public ResourceToModelProfile() 
+        {
+            CreateMap<SaveUserChefResource, UserChef>();
+            CreateMap<SaveUserCommonResource, UserCommon>();
+        }
     }
 }
