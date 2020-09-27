1bbf5ca87e07329dbb1e0606c7e77c28fcfeab33
Aaron Alberto Alva Caffo <u201719762@upc.edu.pe>
2020-09-27 01:42:00 -0500

    UserChefController, UserCommonController

--- Controllers/UserChefController.cs
+++ Controllers/UserChefController.cs
@@ -1,17 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using AutoMapper;
+using Homemade.Domain.Models;
+using Homemade.Domain.Services;
+using Homemade.Extensions;
+using Homemade.Resource;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Homemade.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/[controller]")]
     public class UserChefController : ControllerBase
     {
+        private readonly IUserChefService _userChefService;
+        private readonly IMapper _mapper;
+
+        public UserChefController(IUserChefService userChefService, IMapper mapper)
+        {
+            _userChefService = userChefService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<UserChefResource>> GetAllAsync() 
+        {
+            var userChefs = await _userChefService.ListAsync();
+            var resource = _mapper
+                .Map<IEnumerable<UserChef>, IEnumerable < UserChefResource >> (userChefs);
+            return resource;
+        }
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody] SaveUserChefResource resource)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState.GetErrorMessages());
+
+            var UserChef = _mapper.Map<SaveUserChefResource, UserChef>(resource);
+
+            var result = await _userChefService.SaveAsync(UserChef);
+
+            if (!result.Succes)
+                return BadRequest(result.Message);
+            var userChefResource = _mapper.Map<UserChef, UserChefResource>(result.Resource);
+            return Ok(userChefResource );
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PutAsync(int id, [FromBody] SaveUserChefResource resource)
+        {
+            var userChef = _mapper.Map<SaveUserChefResource, UserChef>(resource);
+            var result = await _userChefService.UpdateAsync(id, userChef);
+            if (!result.Succes)
+                return BadRequest(result.Message);
+            var userChefResource = _mapper.Map<UserChef, UserChefResource>(result.Resource);
+            return Ok(userChefResource);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> DeleteAsync(int id) 
+        {
+            var result = await _userChefService.DeleteAsync(id);
+            if (!result.Succes)
+                return BadRequest(result.Message);
+            var userChefResource = _mapper.Map<UserChef, UserChefResource>(result.Resource);
+            return Ok(userChefResource);
+        }
+
+
         // GET: api/<UserChefController>
         [HttpGet]
         public IEnumerable<string> Get()
--- Controllers/UserCommonController.cs
+++ Controllers/UserCommonController.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using Homemade.Domain.Models;
+using Homemade.Domain.Services;
+using Homemade.Extensions;
+using Homemade.Resource;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -12,6 +17,69 @@ namespace Homemade.Controllers
     [ApiController]
     public class UserCommonController : ControllerBase
     {
+        private readonly IUserCommonService _userCommonService;
+        private readonly IMapper _mapper;
+
+        public UserCommonController(IUserCommonService userCommonService, IMapper mapper)
+        {
+            _userCommonService = userCommonService;
+            _mapper = mapper;
+        }
+
+
+        [HttpGet]
+        public async Task<IEnumerable<UserCommonResource>> GetAllAsync() 
+        {
+            var userCommon = await _userCommonService.ListAsync();
+            var resource = _mapper
+                .Map<IEnumerable<UserCommon>, IEnumerable<UserCommonResource>>(userCommon);
+            return resource;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody] SaveUserCommonResource resource)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState.GetErrorMessages());
+
+            var userCommon = _mapper.Map<SaveUserCommonResource, UserCommon>(resource);
+
+            var result = await _userCommonService.SaveAsync(userCommon);
+
+            if (!result.Succes)
+                return BadRequest(result.Message);
+            var userCommonResource = _mapper.Map<UserCommon, UserCommonResource>(result.Resource);
+            return Ok(userCommonResource);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PutAsync(int id, [FromBody] SaveUserCommonResource resource)
+        {
+            var userCommon = _mapper.Map<SaveUserCommonResource, UserCommon>(resource);
+            var result = await _userCommonService.UpdateAsync(id, userCommon);
+            if (!result.Succes)
+                return BadRequest(result.Message);
+            var userCommonResource = _mapper.Map<UserCommon, UserCommonResource>(result.Resource);
+            return Ok(userCommonResource);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var result = await _userCommonService.DeleteAsync(id);
+            if (!result.Succes)
+                return BadRequest(result.Message);
+            var userCommonResource = _mapper.Map<UserCommon, UserCommonResource>(result.Resource);
+            return Ok(userCommonResource);
+        }
+
+
+
+
+
+
+
+
         // GET: api/<UserCommonController>
         [HttpGet]
         public IEnumerable<string> Get()
