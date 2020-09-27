4c1ff6e206c46146c56fb71ff4633239247ef161
Aaron Alberto Alva Caffo <u201719762@upc.edu.pe>
2020-09-27 17:42:21 -0500

    CommonUserController

---
+++ Controllers/CommonChefController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using AutoMapper;
+using Homemade.Domain.Models;
+using Homemade.Domain.Services;
+using Homemade.Extensions;
+using Homemade.Resource;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homemade.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CommonChefController : ControllerBase
+    {
+        private readonly IUserChefService _userChefService;
+        private readonly ICommonChefService _commonChefService;
+        private readonly IMapper _mapper;
+
+        public CommonChefController(IUserChefService userChefService, ICommonChefService commonChefService, IMapper mapper)
+        {
+            _userChefService = userChefService;
+            _commonChefService = commonChefService;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<UserChefResource>> GetAllByUsersCommondAsync(int userCommonId)
+        {
+            var userChefs = await _userChefService.ListByUserCommonId(userCommonId);
+            var resource = _mapper
+                .Map<IEnumerable<UserChef>, IEnumerable<UserChefResource>>(userChefs);
+            return resource;
+        }
+
+    }
+}
