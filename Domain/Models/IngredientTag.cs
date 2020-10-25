38073bb33b05890cfd45e742c99f5b7b21905dcd
Pacoros98 <65671832+Pacoros98@users.noreply.github.com>
2020-10-25 15:27:34 -0600

    Ingredient Modification

---
+++ Domain/Models/IngredientTag.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Homemade.Domain.Models
+{
+    public class IngredientTag
+    {
+        public int IngredientId { get; set; }
+        public Ingredient Ingredient { get; set; }
+
+        public int TagId { get; set; }
+        public TagMode Tag { get; set; }
+        
+    }
+}
