2eda6295e23d6fe398a8372dd87170d38c32d48f
Pacoros98 <65671832+Pacoros98@users.noreply.github.com>
2020-10-25 18:50:04 -0600

    Payment

--- Domain/Models/Payment.cs
+++ Domain/Models/Payment.cs
@@ -7,5 +7,11 @@ namespace Homemade.Domain.Models
 {
     public class Payment
     {
+        public int Id { get; set; }
+        public int CardNumber { get; set; }
+        public string CardName { get; set; }
+        public string PaymentDetail { get; set; }
+        public DateTime Date { get; set; }
+        public float Total { get; set; }
     }
 }
