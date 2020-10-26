20f3f0dba9c7249b6c5ae663bbd1fcf1a21da5a0
Aaron Alberto Alva Caffo <34250041+CaffoAaron@users.noreply.github.com>
2020-10-26 10:15:45 -0500

    RecipeSteps IRepository

--- Domain/Repositories/IRecipeStepsRepository.cs
+++ Domain/Repositories/IRecipeStepsRepository.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using Homemade.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Homemade.Domain.Repositories
 {
-    interface IRecipeStepsRepository
+    public interface IRecipeStepsRepository
     {
+        Task<IEnumerable<Comment>> ListByRecipeIdAsync(int recipeId);
+        Task AddAsync(Recipe recipe);
+        Task<Recipe> FindById(int id);
+        void Update(Recipe recipe);
+        void Remove(Recipe recipe);
     }
 }
