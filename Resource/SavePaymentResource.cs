9d6cded4061d77b0f0254476d648517f544ac492
Victor Fajardo <victor.fajardo.rojas@gmail.com>
2020-10-26 12:57:22 -0500

    Merge

---
+++ Resource/SavePaymentResource.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Homemade.Resource
+{
+    public class SavePaymentResource
+    {
+        [Required]
+        public int CardNumber { get; set; }
+        public string CardName { get; set; }
+        public string PaymentDetail { get; set; }
+        public DateTime Date { get; set; }
+        public float Total { get; set; }
+    }
+}
---
+++ Resource/SaveRecipeStepsResource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Homemade.Resource
+{
+    public class SaveRecipeStepsResource
+    {
+        [Required]
+        [MaxLength(200)]
+        public string Instructions { get; set; }
+        public Byte[] Picture { get; set; }
+    }
+}
