4a80273a6f8e24911152498bf5d04ede45ac6d70
SanchezD1998 <44213289+SanchezD1998@users.noreply.github.com>
2020-10-25 22:04:51 -0500

    Publication Mapping

--- Mapping/ResourceToModelProfile.cs
+++ Mapping/ResourceToModelProfile.cs
@@ -15,6 +15,7 @@ namespace Homemade.Mapping
             CreateMap<SaveIngredientResource, Ingredient>();
             CreateMap<SaveUserChefResource, UserChef>();
             CreateMap<SaveUserCommonResource, UserCommon>();
+            CreateMap<SavePublicationResource, PublicationResource>();
             CreateMap<SaveCommentResource, CommentResource>();
             CreateMap<SaveRecipeResource, RecipeResource>();
         }
